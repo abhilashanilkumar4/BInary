43af38dd2fbbb38fc293ebfd29632b1e53b99a57
abhilash <124899375+abhilashanilkumar4@users.noreply.github.com>
2023-05-05 00:46:01 +0530

    Add files via upload

---
+++ binary_tree2.py
@@ -0,0 +1,43 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BinaryTree:
+    def __init__(self, root):
+        self.root = Node(root)
+
+    def preorder(self, node):
+        if node:
+            print(node.value, end=' ')
+            self.preorder(node.left)
+            self.preorder(node.right)
+
+    def inorder(self, node):
+        if node:
+            self.inorder(node.left)
+            print(node.value, end=' ')
+            self.inorder(node.right)
+
+    def postorder_traversal(self, node):
+        if node:
+            self.postorder_traversal(node.left)
+            self.postorder_traversal(node.right)
+            print(node.value, end=' ')
+
+
+tree = BinaryTree(1)
+tree.root.left = Node(2)
+tree.root.right = Node(3)
+tree.root.left.left = Node(4)
+tree.root.left.right = Node(5)
+tree.root.right.left = Node(6)
+tree.root.right.right = Node(7)
+
+print("\nPre-order:", end=' ')
+tree.preorder(tree.root)
+print("\nIn-order:", end=' ')
+tree.inorder(tree.root)
+print("\nPost-order:", end=' ')
+tree.postorder_traversal(tree.root)

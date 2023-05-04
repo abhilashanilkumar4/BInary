4679825817f56314feebd3dc44c728db43787679
abhilash <124899375+abhilashanilkumar4@users.noreply.github.com>
2023-05-05 00:38:16 +0530

    Add files via upload

---
+++ binary_tree1.py
@@ -0,0 +1,28 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BinaryTree:
+    def __init__(self, root):
+        self.root = Node(root)
+
+    def height(self, node):
+        if node is None:
+            return 0
+        else:
+            left_height = self.height(node.left)
+            right_height = self.height(node.right)
+            return max(left_height, right_height) + 1
+
+
+tree = BinaryTree(1)
+tree.root.left = Node(2)
+tree.root.right = Node(3)
+tree.root.left.left = Node(4)
+tree.root.left.right = Node(5)
+tree.root.right.left = Node(6)
+tree.root.right.right = Node(7)
+
+print("Height of the tree is:", tree.height(tree.root))
